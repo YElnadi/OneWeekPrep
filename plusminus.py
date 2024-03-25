bc841198f15230b085868de8cd97a93e716b51f7
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-03-25 03:25:48 +0200

    done

--- plusminus.py
+++ plusminus.py
@@ -3,3 +3,20 @@ Given an array of integers, calculate the ratios of its elements that are positi
 
 Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.
 '''
+
+def plusMinus(arr):
+    # Write your code here
+    arr_len = len(arr)
+    pos = 0
+    neg = 0
+    zero = 0
+    for ele in arr:
+        if ele < 0:
+            neg += 1
+        if ele > 0:
+            pos += 1
+        if ele == 0:
+            zero += 1
+    print("{0:.6f}".format(pos/arr_len))  
+    print("{0:.6f}".format(neg/arr_len))   
+    print("{0:.6f}".format(zero/arr_len))     
