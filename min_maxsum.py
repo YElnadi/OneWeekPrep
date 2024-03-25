63b0d9e124ea7f44b80f01b45875c3870cdc85cb
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-03-25 03:48:30 +0200

    done

--- min_maxsum.py
+++ min_maxsum.py
@@ -1,3 +1,6 @@
+'''
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+'''
 def miniMaxSum(arr):
     # Write your code here
     min_sum = 0
