9c5687ada128baf25445a9bdefc605bbe6491383
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-04-07 02:59:31 +0200

    done

--- diagonalDiff.py
+++ diagonalDiff.py
@@ -2,8 +2,34 @@
 Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 arr = 1 2 3
       4 5 6
-      7 8 9
+      9 8 9
 '''
 
 def diagonalDifference(arr):
-    pass
+    left_to_right = 0
+    right_to_left = 0
+    n = len(arr)
+    for r in range(n):
+        left_to_right += arr[r][r]
+        right_to_left += arr[r][n - r - 1]
+
+    return abs(left_to_right - right_to_left)
+    
+    
+
+
+
+print(diagonalDifference([[1,2,3],[4,5,6],[7,8,9]]))
+
+# def diagonalDifference(arr):
+#     left_to_right = 0
+#     right_to_left = 0
+#     n = len(arr)
+#     for r in range(n):
+#         left_to_right += arr[r][r]
+#         right_to_left += arr[r][n-r-1]
+#     print("Left to right diagonal sum:", left_to_right)
+#     print("Right to left diagonal sum:", right_to_left)
+#     return abs(left_to_right - right_to_left)
+
+# print(diagonalDifference([[1,2,3],[4,5,6],[9,8,9]]))
