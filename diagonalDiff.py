0b157c4c22bcdb5f943ce1343748cad97fac6274
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-04-07 02:41:31 +0200

    in progress

---
+++ diagonalDiff.py
@@ -0,0 +1,9 @@
+'''
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+arr = 1 2 3
+      4 5 6
+      7 8 9
+'''
+
+def diagonalDifference(arr):
+    pass
