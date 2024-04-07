d582740a78f6ed78cba22054cbec6f89215c67a2
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-04-07 03:04:45 +0200

    inprogress

---
+++ countingSort_1.py
@@ -0,0 +1,4 @@
+'''
+Quicksort usually has a running time of nX log(n) , but is there an algorithm that can sort even faster? In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm cannot beat nX log(n) (worst-case) running time, since nX log(n) represents the minimum number of comparisons needed to know where to place each element. For more details, you can see these notes (PDF).
+
+'''
