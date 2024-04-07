3ed8ffe002efa2616c1b88ed2393ca9ed10c72ea
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-04-07 02:27:32 +0200

    done

--- lonelyInteger.py
+++ lonelyInteger.py
@@ -4,5 +4,16 @@ example: a = [1,2,3,4,3,2,1]
 answer is 4
 '''
 def lonelyinteger(a):
-    # Write your code here
-    pass
+    dict_arr = {}
+    for num in a:
+        if num not in dict_arr:
+            dict_arr[num] = 1
+        else:
+            dict_arr[num] += 1
+    # return dict_arr
+
+    for key, value in dict_arr.items():
+        if value == 1:
+            return key
+
+print(lonelyinteger([1,2,3,4,3,2,1]))
