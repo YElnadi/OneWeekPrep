4cf0b163bc788bd42b7ed6775671b3e850fc6f32
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-04-07 02:16:58 +0200

    in progress

---
+++ lonelyInteger.py
@@ -0,0 +1,8 @@
+'''
+Given an array of integers, where all elements but one occur twice, find the unique element.
+example: a = [1,2,3,4,3,2,1]
+answer is 4
+'''
+def lonelyinteger(a):
+    # Write your code here
+    pass
