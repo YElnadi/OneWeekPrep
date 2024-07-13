ec7bbd64e6a6f59fd1fb472749980be8863fc812
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-07-13 01:45:54 -0700

    solved

--- countCode.py
+++ countCode.py
@@ -4,3 +4,12 @@
 # count_code('codexxcode') → 2
 # count_code('cozexxcope') → 2
 
+def count_code(s):
+    count_code = 0
+    for i in range(len(s) - 3):
+        if s[i:i+2] == 'co' and s[i+3] == 'e':
+            count_code += 1
+    return count_code
+
+
+print(count_code('cozexxcope'))
