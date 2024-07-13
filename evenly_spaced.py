288f369459ea748663ef72723a1156521e35cf2f
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-07-13 01:06:33 -0700

    solved

--- evenly_spaced.py
+++ evenly_spaced.py
@@ -5,3 +5,13 @@
 # evenlySpaced(4, 6, 2) → true
 # evenlySpaced(4, 6, 3) → false
 
+def evenlySpaced(a, b, c):
+    sorted_nums = sorted([a,b,c])
+    diff1 = sorted_nums[1] - sorted_nums[0]
+    diff2 = sorted_nums [2] - sorted_nums[1]
+    return diff1 == diff2
+
+
+print(evenlySpaced(2, 4, 6))
+print(evenlySpaced(4, 6, 2))
+print(evenlySpaced(4, 6, 3))
