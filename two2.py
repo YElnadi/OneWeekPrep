ccf071fdd0f8c653ec9e0c5b95f5448c26397e2f
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-07-16 14:44:17 -0700

    done

--- two2.py
+++ two2.py
@@ -5,3 +5,24 @@
 # two2([0]) → [0]
 
 def two2(nums):
+     #[1,2,3] -> 1x2 = 2(omitted because it ends with 2 - 02 ) 2x2= 4 3x2=6. 
+    #[2,6,11] -> 4, 12(omitted), 22(omitted)
+    arr = []
+
+    # for i in nums:
+    #     res = i * 2
+    #     if str(res) != '2' and not str(res).endswith('2'):
+    #         arr.append(res)
+    # return arr
+
+    for i in nums:
+        res = i * 2
+        if res % 10 != 2:
+            arr.append(res)
+
+    return arr
+
+
+print(two2([1, 2, 3])) 
+print(two2([2, 6, 11]))
+print(two2([0]))
